a13e8e35cef48dd584e98ef1ca6be7f8805e46d3
unknown <kaitlandevans@gmail.com>
2022-05-04 12:02:24 -0500

    created persons objects and for of loop

--- main.js
+++ main.js
@@ -34,30 +34,21 @@ const person = {
 console.log(Object.entries(person))
 
 // Create an arrayOfPersons that contains multiple "people" objects. You can simplify copy/paste the person object you made above multiple times. Feel free to change the values to reflect multiple people you might have in your database.
-const arrayOfPersons = {
-  [
-    const person1 = {
-      firstName: "Jane",
-      lastName: "Doe",
-      birthDate: "Jan 5, 1925",
-      gender: "female"
-    },
-    const person2 = {
-      firstName: "John",
-      lastName: "Doe",
-      birthDate: "Jan 25, 1905",
-      gender: "male"
-    },
-    const person3 = {
-      firstName: "Baby",
-      lastName: "Doe",
-      birthDate: "Jan 5, 1945",
-      gender: "female"
-     }
+const arrayOfPersons = [
+     person1 = {firstName: "Jane", lastName: "Doe", birthDate: "Jan 5, 1925", gender: "female"},
+     person2 = {firstName: "John", lastName: "Doe", birthDate: "Jan 25, 1908", gender: "male"},
+     person3 = {firstName: "Baby", lastName: "Doe", birthDate: "June 17, 1945", gender: "female"}
 ]
+// console.log(arrayOfPersons)
+// Create a function that uses a for..of loop and an if statement to console.log the value 
+// associated with the key birthDate of each object if the birth year is an odd number.
+const loopOverArray = (arrayOfPersons) => {
+  for (birthDate of arrayOfPersons) {
+    if (arrayOfPersons.entries(birthDate) % 2 === 0){
+    // console.log(birthDate)}
+  }
 }
-// Create a function that uses a for..of loop and an if statement to console.log the value associated with the key birthDate of each object if the birth year is an odd number.
-
+console.log(loopOverArray(birthDate))
 // Use .map() to map over the arrayOfPersons and console.log() their information.
 
 // Use .filter() to filter the persons array and console.log 0only males in the array.
