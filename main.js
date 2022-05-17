12730a8f55aa8bfd440fa57105bd2fdb1d4dbebf
unknown <kaitlandevans@gmail.com>
2022-05-17 18:36:46 -0500

    Still need to console the last filter correctly

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ console.log(Object.entries(person))
 // Create an arrayOfPersons that contains multiple "people" objects. You can simplify copy/paste the person object you made above multiple times. Feel free to change the values to reflect multiple people you might have in your database.
 const arrayOfPersons = [
      person1 = {firstName: "Jane", lastName: "Doe", birthDate: "Jan 5, 1925", gender: "female"},
-     person2 = {firstName: "John", lastName: "Doe", birthDate: "Jan 25, 1908", gender: "male"},
+     person2 = {firstName: "John", lastName: "Doe", birthDate: "Jan 25, 1992", gender: "male"},
      person3 = {firstName: "Baby", lastName: "Doe", birthDate: "June 17, 1945", gender: "female"}
 ]
 // console.log(arrayOfPersons)
@@ -58,12 +58,30 @@ const loopOverArray = () => {
 }
 loopOverArray()
 // console.log(loopOverArray(birthDate))
+
+
 // Use .map() to map over the arrayOfPersons and console.log() their information.
-
+const map1 = arrayOfPersons.map(x => x)
+console.log(map1)
 // Use .filter() to filter the persons array and console.log 0only males in the array.
-
+let maleOnly = arrayOfPersons.filter(y => y.gender == "male")
+    console.log(maleOnly) 
+  
 // Create a function that returns true if the value of birthDate is before Jan 1, 1990.
-
+const birthBefore90 = () => {
+  let beforeDate = arrayOfPersons.filter(z => z.birthDate <= "Jan 1, 1990")
+  for (birthDate of arrayOfPersons){
+     if (beforeDate){
+    console.log(beforeDate)
+    return true
+  }
+    else {
+      console.log()
+      return false
+    }
+  }
+}
+console.log(birthBefore90())
 // Use .filter() to filter the persons array and console.log only people that were born before Jan 1, 1990.
 
 // BONUS - Create a function that returns true if the date passed to it is >= 21 years in the past.
